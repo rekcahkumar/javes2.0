42639f46be5968cd078500db72b37ddb2b9c57fe
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-08-17 09:57:03 +0530

    Update __init__.py

--- userbot/database/__init__.py
+++ userbot/database/__init__.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from userbot import DB_URI
+
 
 BASE = declarative_base()
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI)
+    engine = create_engine(None)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
